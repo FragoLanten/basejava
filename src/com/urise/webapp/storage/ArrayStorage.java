614e54f7cb4144432d846ce4bf5eb7d2b47ea9ab
FragoLanten <rizinvadim@gmail.com>
2023-03-06 13:32:57 +0300

    ДЗ02 рефакторинг кода, ввел переменную storage limit. отрефакторил методы save, get, delete, getall, getIndex

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    final int STORAGE_LIMIT = 10000;
+    final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
     public void clear() {
@@ -17,26 +19,35 @@ public class ArrayStorage {
     }
 
     public void update(Resume resume) {
-        int indexOfResume = findIndexOfResume(resume);
-        if (indexOfResume >= 0) {
-            storage[indexOfResume] = resume;
+        int index = getIndex(resume);
+        if (index >= 0) {
+            storage[index] = resume;
         } else {
             System.out.println("Resume with " + resume.getUuid() + " is not present in storage");
         }
     }
 
     public void save(Resume r) {
-        if (findIndexOfResume(r) < 0) {
-            if (size < 10000) {
-                storage[size] = r;
-                size++;
-            } else System.out.println("Excess the size of storage");
-        } else System.out.println("Resume with " + r.getUuid() + " is already present in storage");
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("Excess the size of storage");
+        } else if (getIndex(r) >= 0) {
+            System.out.println("Resume with " + r.getUuid() + " is already present in storage");
+        } else {
+            storage[size] = r;
+            size++;
+        }
     }
 
     public Resume get(String uuid) {
-        if (findResumeByUuid(uuid) != null) {
-            return findResumeByUuid(uuid);
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            return storage[index];
         } else {
             System.out.println("Resume with " + uuid + " is not present in storage");
             return null;
@@ -44,42 +55,33 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-        if (findResumeByUuid(uuid) != null) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                    size--;
-                    break;
-                }
-            }
-        } else System.out.println("Resume with " + uuid + " is not present in storage");
+        int index = -1;
+        Resume resume = get(uuid);
+        if (resume != null) {
+            index = getIndex(resume);
+        }
+        if (index != -1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] allResume = new Resume[size];
-        System.arraycopy(storage, 0, allResume, 0, size);
-        return allResume;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
     }
 
-    int findIndexOfResume(Resume resume) {
+    int getIndex(Resume resume) {
         Arrays.sort(storage, 0, size);
-        return Arrays.binarySearch(storage, 0, size, resume);
-    }
-
-    Resume findResumeByUuid(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
-            }
-        }
-        return null;
+        if (Arrays.binarySearch(storage, 0, size, resume) >= 0) {
+            return Arrays.binarySearch(storage, 0, size, resume);
+        } else return -1;
     }
 }
