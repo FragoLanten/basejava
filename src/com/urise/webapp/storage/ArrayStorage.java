cb2794df1520b86dc453453416129921d0797eb4
FragoLanten <rizinvadim@gmail.com>
2023-03-12 03:33:44 +0300

    ДЗ03 сделал класс SortedArrayStorage. Написал для него свои методы. Все методы которые имеют общую реализацию оставил в AbsractArrayStorage с дефолтной реализацией. ArrayStorage почти не менял.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,16 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-
-    final int STORAGE_LIMIT = 10000;
-    final Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size;
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void update(Resume resume) {
         int index = getIndex(resume.getUuid());
@@ -26,7 +17,6 @@ public class ArrayStorage {
             System.out.println("Resume with " + resume.getUuid() + " is not present in storage");
         }
     }
-
     public void save(Resume r) {
         if (size >= STORAGE_LIMIT) {
             System.out.println("Excess the size of storage");
@@ -35,16 +25,6 @@ public class ArrayStorage {
         } else {
             storage[size] = r;
             size++;
-        }
-    }
-
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume with " + uuid + " is not present in storage");
-            return null;
-        } else {
-            return storage[index];
         }
     }
 
@@ -59,18 +39,7 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    int getIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
